cc2287ebe0d449e1c36292db8eae628ef8576d0f
khanhpro9889 <khanhpro9889@gmail.com>
2022-08-05 22:53:04 +0700

    fix eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import { useEffect } from 'react';
-import { addToCart, deleteFromCart, addQuantity, removeQuantity } from './redux/actions/cartActions/cartActions';
-import { useDispatch, useSelector } from 'react-redux';
+import "./App.css";
+import { useEffect } from "react";
+import {
+  addToCart,
+  deleteFromCart,
+  addQuantity,
+  removeQuantity,
+} from "./redux/actions/cartActions/cartActions";
+import { useDispatch, useSelector } from "react-redux";
 //import { fetchBag } from './redux/actions/bagActions/bagActions';
-import { fetchBagApi } from './redux/actions/bagActions/bagActions';
+import { fetchBagApi } from "./redux/actions/bagActions/bagActions";
 
-import React from 'react'
+import React from "react";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,64 +19,84 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchBagApi());
-  }, [])
+    // eslint-disable-next-line
+  }, []);
 
   const addToCartFunc = (item) => {
     dispatch(addToCart(item));
-  }
+  };
 
   const deleteFromCartFunc = (item) => {
     dispatch(deleteFromCart(item));
-  }
+  };
 
   const addQuantityFunc = (item) => {
     dispatch(addQuantity(item));
-  }
+  };
 
   const removeQuantityFunc = (item) => {
     dispatch(removeQuantity(item));
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="container">
-          {
-            bag.loading ? <div>Loading</div> : bag.list.map(item => {
+          {bag.loading ? (
+            <div>Loading</div>
+          ) : (
+            bag.list.map((item) => {
               return (
                 <div className="box">
                   <div>{item.name}</div>
-                  <button onClick={() => {
-                    addToCartFunc(item);
-                  }}>Add to cart</button>
+                  <button
+                    onClick={() => {
+                      addToCartFunc(item);
+                    }}
+                  >
+                    Add to cart
+                  </button>
                 </div>
-              )
+              );
             })
-          }
+          )}
         </div>
         <hr />
-        <div><h2>Cart</h2></div>
+        <div>
+          <h2>Cart</h2>
+        </div>
         <div className="container">
-          {
-            cart && cart.items.map(i => {
+          {cart &&
+            cart.items.map((i) => {
               return (
                 <div className="box">
                   <div>{i.item.name}</div>
                   <div>Quanity: {i.quantity}</div>
-                  <button onClick={() => {
-                    deleteFromCartFunc(i.item);
-                  }}>Remove</button>
-                  <button onClick={() => {
-                    addQuantityFunc(i.item);
-                  }}>+</button>
-                  <button onClick={() => {
-                    removeQuantityFunc(i.item);
-                  }}>-</button>
+                  <button
+                    onClick={() => {
+                      deleteFromCartFunc(i.item);
+                    }}
+                  >
+                    Remove
+                  </button>
+                  <button
+                    onClick={() => {
+                      addQuantityFunc(i.item);
+                    }}
+                  >
+                    +
+                  </button>
+                  <button
+                    onClick={() => {
+                      removeQuantityFunc(i.item);
+                    }}
+                  >
+                    -
+                  </button>
                   <br />
                 </div>
-              )
-            })
-          }
+              );
+            })}
         </div>
       </header>
     </div>
